043e7691cfb74de51ee26c9925076ad7aba5ad3a
peterlidee <peter@lidee.be>
2023-04-28 15:56:22 +0200

    Added Page wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
-import 'normalize.css';
-import '../styles/index.scss';
+import 'normalize.css'
+import '../styles/index.scss'
+import Page from '@/components/Page'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Page>
+      <Component {...pageProps} />
+    </Page>
+  )
 }

cb7aaff87170881acb236f5df0601ef5d7bc66dd
peterlidee <peter@lidee.be>
2023-04-28 15:58:28 +0200

    updated document with gfonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,16 @@
+// pages/_document.js
+// https://nextjs.org/docs/messages/no-page-custom-font
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <link
+          href='https://fonts.googleapis.com/css2?family=Inconsolata&family=Quicksand:wght@400;500;700&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />

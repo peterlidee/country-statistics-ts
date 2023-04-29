fdd90ece7f807b9352c8516097497440ccf1b0e8
peterlidee <peter@lidee.be>
2023-04-29 18:33:56 +0200

    Added country type

---
+++ types/country.d.ts
@@ -0,0 +1,12 @@
+export type Country = {
+  countryName: string
+  cca3: string
+  region: string
+  subregion: string
+  area: number
+  areaPrettyFormat: string
+  population: number
+  populationPrettyFormat: string
+  density: number
+  densityPrettyFormat: string
+}

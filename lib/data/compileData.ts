c2eda2084124850ae7882df61dc4f44e8ca625d2
peterlidee <peter@lidee.be>
2023-04-30 17:15:26 +0200

    Added and converted compileData

---
+++ lib/data/compileData.ts
@@ -0,0 +1,65 @@
+import { Country } from '@/types/country'
+import formatNumber from '../helpers/formatNumber'
+import roundNumber from '../helpers/roundNumber'
+
+// this function compiles the data from the raw data
+// [A] it copies all required data to (simpler) props
+// [B] it add some extra data
+// [A] copy required data to simpler props
+// [B] takes arr of object, for each object add properties: 1,2,3
+// 1. density and densityPrettyFormat if area && population
+// 2. areaPrettyFormat if area
+// 3. populationPrettyFormat if population
+// 4. countryName added as extra field (makes it easier to sort)
+// 5. replace some faulty data
+
+export default function compileData(arr: any): Country[] {
+  // console.log('addExtraData called',)
+
+  return arr.map((item: any) => {
+    // purely copy some props
+    const itemCopy: Country = {
+      countryName: item?.name?.common || '',
+      cca3: item.cca3,
+      region: item.region,
+      subregion: item.subregion,
+      area: 0,
+      areaPrettyFormat: '0',
+      population: 0,
+      populationPrettyFormat: '0',
+      density: 0,
+      densityPrettyFormat: '0',
+    }
+
+    // area
+    if (item.hasOwnProperty('area')) {
+      // add area
+      itemCopy.area = roundNumber(item.area)
+      // add areaPrettyFormat
+      itemCopy.areaPrettyFormat = formatNumber(itemCopy.area)
+    }
+
+    // population
+    if (item.hasOwnProperty('population')) {
+      // add population
+      itemCopy.population = roundNumber(item.population)
+      // add populationPrettyFormat
+      itemCopy.populationPrettyFormat = formatNumber(itemCopy.population)
+    }
+
+    // density
+    if (item.hasOwnProperty('area') && item.hasOwnProperty('population')) {
+      // add population density
+      itemCopy.density = roundNumber(item.population / item.area)
+      // add population densityToDisplay
+      itemCopy.densityPrettyFormat = formatNumber(itemCopy.density)
+    }
+
+    // 5. faulty data
+    // Åland Islands get sorted incorrrectly!! so flip Å with A
+    if (itemCopy.cca3 == 'ALA') {
+      itemCopy.countryName = 'Aland Islands'
+    }
+    return itemCopy
+  })
+}

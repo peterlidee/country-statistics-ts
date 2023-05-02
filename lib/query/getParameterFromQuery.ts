f950c2809fe4bc86e3c25228c75061390580a37e
peterlidee <peter@lidee.be>
2023-05-02 20:47:36 +0200

    Added 'regions' to parameter literal union type

--- lib/query/getParameterFromQuery.ts
+++ lib/query/getParameterFromQuery.ts
@@ -2,18 +2,20 @@ import { ParsedUrlQuery } from 'querystring'
 
 /**
  * check if @query has @param and it's value is not empty
- * @param parameter literal union
+ * @param parameter literal //todo
  * @param query router.query
- * @returns - [] or string[]
+ * @returns - string[]
  */
 
 export default function getParameterFromQuery(
-  parameter: 'hide',
+  parameter: 'hide' | 'regions',
   query: ParsedUrlQuery,
-): string[] {
+) {
   if (!query.hasOwnProperty(parameter) || query[parameter] === '') {
     return []
   }
-  const queryString = query[parameter] as string
-  return queryString.split(',')
+  const queryString = query[parameter]
+  // narrow type, we only work with string ,ParsedUrlQuery offers more options
+  if (typeof queryString === 'string') return queryString.split(',')
+  return []
 }

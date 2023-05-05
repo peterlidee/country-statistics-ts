967ac99db0dd37db9c99db08f280670ee4992c2f
peterlidee <peter@lidee.be>
2023-05-05 17:09:20 +0200

    Updated function names

--- components/fields/types/fieldsPredicates.ts
+++ components/fields/types/fieldsPredicates.ts
@@ -2,12 +2,12 @@
 
 import { NumberField, TextField } from './fields'
 
-// check if field is NumberFieldDataType
-export const isNumberFieldData = (
+// check if field is NumberField
+export const isNumberField = (
   fieldData: NumberField | TextField,
 ): fieldData is NumberField => fieldData.sortType === 'number'
 
-// check if field is TextFieldDataType
-export const isTextFieldData = (
+// check if field is TextField
+export const isTextField = (
   fieldData: NumberField | TextField,
 ): fieldData is TextField => fieldData.sortType === 'text'

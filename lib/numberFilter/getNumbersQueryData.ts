546e00bb2060145479ad9d3fc42de03a61ee3c04
peterlidee <peter@lidee.be>
2023-05-05 17:37:25 +0200

    updated import and renamed a lot of vars

--- lib/numberFilter/getNumbersQueryData.ts
+++ lib/numberFilter/getNumbersQueryData.ts
@@ -1,9 +1,10 @@
-import fieldsData from '../../components/fields/fieldsData'
 import validateNumbersQuery from './validateNumbersQuery'
 import isNumberFilterActive from './isNumberFilterActive'
 import { ParsedUrlQuery } from 'querystring'
 import { FilterDataType } from '@/types/filterData'
-import { NumberFieldSlug, isNumberFieldData } from '@/types/fieldsData'
+import fields from '@/components/fields/fields'
+import { NumberFieldSlug } from '@/components/fields/types/fields'
+import { isNumberField } from '@/components/fields/types/fieldsPredicates'
 
 // this function returns
 // - activeNumberFilters: array of string
@@ -15,19 +16,17 @@ export default function getNumberQueryData(
   filterData: FilterDataType,
 ) {
   // 1. get NumberFieldsData
-  const numberFieldsData = fieldsData.filter(isNumberFieldData)
+  const numberFields = fields.filter(isNumberField)
 
   // 2. filter our the hidden fields (we know what's hidden from hide router.query)
-  const visibleNumberFieldsData = numberFieldsData.filter((numberFieldData) =>
-    hiddenFields.includes(numberFieldData.slug),
+  const visibleNumberFields = numberFields.filter((numberField) =>
+    hiddenFields.includes(numberField.slug),
   )
 
-  // 3. get the slugs of visible NumberFieldsData
-  const visibleNumberFieldsSlugs = visibleNumberFieldsData.map(
-    (item) => item.slug,
-  )
+  // 3. get the slugs of visible NumberFields
+  const visibleNumberFieldSlugs = visibleNumberFields.map((item) => item.slug)
 
-  // 4. for each visibleNumberFieldsSlugs, get the value from query and validate it (or set to default)
+  // 4. for each visibleNumberFieldSlug, get the value from query and validate it (or set to default)
   // mapped type definition [slug in NumberFieldSlug]
   const currentSelections: { [slug in NumberFieldSlug]: [number, number] } = {
     area: [0, 0],
@@ -35,28 +34,27 @@ export default function getNumberQueryData(
     density: [0, 0],
   }
 
-  visibleNumberFieldsSlugs.map((visibleNumberFieldsSlug) => {
+  visibleNumberFieldSlugs.map((visibleNumberFieldSlug) => {
     const currentSelection = validateNumbersQuery(
-      routerQuery[visibleNumberFieldsSlug] as string | undefined,
+      routerQuery[visibleNumberFieldSlug] as string | undefined,
       [
-        filterData[visibleNumberFieldsSlug].sliderStart,
-        filterData[visibleNumberFieldsSlug].sliderEnd,
+        filterData[visibleNumberFieldSlug].sliderStart,
+        filterData[visibleNumberFieldSlug].sliderEnd,
       ],
     )
     // add to currentSelections
-    currentSelections[visibleNumberFieldsSlug] = currentSelection
+    currentSelections[visibleNumberFieldSlug] = currentSelection
   })
 
   // 5. check which filters in currentSelections are active ones
   // currentSelections returns either a selection or the defaults (min, max)
   // this leaves us without knowledge which filter is actually active
   // we calculate this by comparing the currentSelections with the defaults
-  const activeNumberFilters = visibleNumberFieldsSlugs.filter(
-    (visibleNumberFieldsSlug) =>
-      // we can assert it's not undefined or null because we just made the same loop above, hence !
-      isNumberFilterActive(currentSelections[visibleNumberFieldsSlug], [
-        filterData[visibleNumberFieldsSlug].sliderStart,
-        filterData[visibleNumberFieldsSlug].sliderEnd,
+  const activeNumberFilters = visibleNumberFieldSlugs.filter(
+    (visibleNumberFieldSlug) =>
+      isNumberFilterActive(currentSelections[visibleNumberFieldSlug], [
+        filterData[visibleNumberFieldSlug].sliderStart,
+        filterData[visibleNumberFieldSlug].sliderEnd,
       ]),
   )
 

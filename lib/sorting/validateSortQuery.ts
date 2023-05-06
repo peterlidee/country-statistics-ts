21a6b5a28cdbe2151bf4dd0444e472c2627d4e7d
peterlidee <peter@lidee.be>
2023-05-06 14:58:29 +0200

    Updated function

---
+++ lib/sorting/validateSortQuery.ts
@@ -0,0 +1,41 @@
+import {
+  isNumberFieldSlug,
+  isTextFieldSlug,
+} from '@/components/fields/types/fieldsPredicates'
+import { ValidSortData } from './applySorting'
+
+/**
+ * validates if values[0] is of type NumberFieldSlug or TextFieldSlug
+ * @param values - string[], non empty query.sort string
+ * @param defaultSortData - { sortBy: default, sortAsc: default }
+ * @returns either @defaultSortData when not valid or a valid { sortBy, sortAsc }
+ */
+
+function validateSortQuery(
+  values: string[],
+  defaultSortData: ValidSortData,
+): ValidSortData {
+  // no need to validate when no values
+  if (values.length === 0) return defaultSortData
+
+  // check if there is a '-' in front of the value
+  const valueIsNegated = values[0].startsWith('-')
+  // we are only ever interested in the first value (sorting only works on 1 field at a time)
+  const strippedValue = valueIsNegated ? values[0].slice(1) : values[0]
+
+  if (isNumberFieldSlug(strippedValue) || isTextFieldSlug(strippedValue)) {
+    // value is valid
+    // strippedValue become type NumberFieldSlug | TextFieldSlug
+    // console.log(strippedValue)
+    const sortValueData: ValidSortData = {
+      sortBy: strippedValue,
+      sortAsc: valueIsNegated,
+    }
+    return sortValueData
+  } else {
+    // value is not of type NumberFieldSlug or TextFieldSlug and hence is not valid
+    return defaultSortData
+  }
+}
+
+export default validateSortQuery

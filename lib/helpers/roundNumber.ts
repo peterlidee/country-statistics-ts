5293078db1925028f00f4f994bb6ce19f3c207bc
peterlidee <peter@lidee.be>
2023-05-07 18:42:21 +0200

    Updated roundNumber with comments and updated test

--- lib/helpers/roundNumber.ts
+++ lib/helpers/roundNumber.ts
@@ -1,6 +1,12 @@
-// round the number or return 0 when num is not number or when number is smaller then 0
+/**
+ * Rounds the number or returns 0 (NaN, not number or negative number)
+ * @param num - a number
+ * @returns a rounded number or 0
+ */
+
 export default function roundNumber(num: number | string): number {
   if (typeof num !== 'number') return 0
+  if (isNaN(num)) return 0
   if (num < 0) return 0
   return Math.round(num)
 }

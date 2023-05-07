783bc45633cb089f27ca684cd6020850e1624f41
peterlidee <peter@lidee.be>
2023-05-07 18:32:57 +0200

    updated comments

--- lib/helpers/formatNumber.ts
+++ lib/helpers/formatNumber.ts
@@ -1,9 +1,12 @@
-// take number return formatted number
-// 100000 -> 100.000
-function formatNumber(num: number): string {
-  // format number
-  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString
+/**
+ * Takes number and returns formatted string
+ * f.e. 100000 returns '100.000'
+ * @param num - a number
+ * @returns - a formated string
+ */
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString
+
+export default function formatNumber(num: number): string {
   return num.toLocaleString('nl-BE')
 }
-
-export default formatNumber

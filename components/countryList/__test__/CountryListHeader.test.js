24e0c51b88c848c7f3f434283e69f7c7f1f16c88
peterlidee <peter@lidee.be>
2023-05-09 17:40:53 +0200

    Updated countryListHeader test

--- components/countryList/__test__/CountryListHeader.test.js
+++ components/countryList/__test__/CountryListHeader.test.js
@@ -5,85 +5,89 @@ import Wrapper from '../../general/Wrapper'
 import IconSort from '../../svgSnippets/IconSort'
 
 jest.mock('next/router', () => ({
-  useRouter: jest.fn(() => ({ query: {} }))
+  useRouter: jest.fn(() => ({ query: {} })),
 }))
 jest.mock('../../general/Wrapper', () => {
   return jest.fn((props) => <>{props.children}</>)
 })
 jest.mock('../../svgSnippets/IconSort')
 
-function setup(sortBy, sortAsc){
+function setup(sortBy, sortAsc) {
   render(
-    <CountryListHeader 
-      field={{ 
+    <CountryListHeader
+      field={{
         slug: 'country',
-        label: 'label',
+        label: 'Country',
         sortAscDefault: true,
-      }} 
+      }}
       sortBy={sortBy}
       sortAsc={sortAsc}
-    />
+    />,
   )
   const linkEl = screen.getByRole('link')
   return {
-    linkEl
+    linkEl,
   }
 }
 
 describe('components/countryList/CountryListHeader', () => {
-
   test('It renders', () => {
-    const { linkEl } = setup('foobar', true)
+    const { linkEl } = setup('area', true)
     expect(Wrapper).toHaveBeenCalled()
-    expect(linkEl).toHaveTextContent('label')
+    expect(linkEl).toHaveTextContent('Country')
+    expect(linkEl).toHaveClass('link__sort--country')
     expect(IconSort).toHaveBeenCalled()
   })
 
-  test('It renders with sortBy empty and sort Asc true', () => {
-    const { linkEl } = setup('foobar', true)
-    expect(linkEl).toHaveAttribute('href', '/?sort=country')
-    expect(IconSort).toHaveBeenCalledWith(
-      expect.objectContaining({
-        sortActive: false,
-        sortAsc: true,
-      }),
-      expect.anything()
-    )
+  describe('It renders not active field country correctly', () => {
+    test('It renders field country with sortBy area and sortAsc true', () => {
+      const { linkEl } = setup('area', true)
+      expect(linkEl).toHaveAttribute('href', '/?sort=-country')
+      expect(IconSort).toHaveBeenCalledWith(
+        expect.objectContaining({
+          sortActive: false,
+          sortAsc: true,
+        }),
+        expect.anything(),
+      )
+    })
+
+    test('It renders field country with sortBy area and sort Asc false', () => {
+      const { linkEl } = setup('area', false)
+      expect(linkEl).toHaveAttribute('href', '/?sort=-country')
+      expect(IconSort).toHaveBeenCalledWith(
+        expect.objectContaining({
+          sortActive: false,
+          sortAsc: false,
+        }),
+        expect.anything(),
+      )
+    })
   })
 
-  test('It renders with sortBy empty and sort Asc false', () => {
-    const { linkEl } = setup('foobar', false)
-    expect(linkEl).toHaveAttribute('href', '/?sort=country')
-    expect(IconSort).toHaveBeenCalledWith(
-      expect.objectContaining({
-        sortActive: false,
-        sortAsc: false,
-      }),
-      expect.anything()
-    )
-  })
+  describe('It renders active field country correctly (active = sortBy === field.slug', () => {
+    test('It renders field country with sortBy country and sortAsc true', () => {
+      const { linkEl } = setup('country', true)
+      expect(linkEl).toHaveAttribute('href', '/?sort=country')
+      expect(IconSort).toHaveBeenCalledWith(
+        expect.objectContaining({
+          sortActive: true,
+          sortAsc: true,
+        }),
+        expect.anything(),
+      )
+    })
 
-  test('It renders with sortBy country and sort Asc true', () => {
-    const { linkEl } = setup('country', true)
-    expect(linkEl).toHaveAttribute('href', '/?sort=-country')
-    expect(IconSort).toHaveBeenCalledWith(
-      expect.objectContaining({
-        sortActive: true,
-        sortAsc: true,
-      }),
-      expect.anything()
-    )
+    test('It renders field country with sortBy country and sortAsc false', () => {
+      const { linkEl } = setup('country', false)
+      expect(linkEl).toHaveAttribute('href', '/?sort=-country')
+      expect(IconSort).toHaveBeenCalledWith(
+        expect.objectContaining({
+          sortActive: true,
+          sortAsc: false,
+        }),
+        expect.anything(),
+      )
+    })
   })
-
-  test('It renders with sortBy country and sort Asc false', () => {
-    setup('country', false)
-    expect(IconSort).toHaveBeenCalledWith(
-      expect.objectContaining({
-        sortActive: true,
-        sortAsc: false,
-      }),
-      expect.anything()
-    )
-  })
-
 })

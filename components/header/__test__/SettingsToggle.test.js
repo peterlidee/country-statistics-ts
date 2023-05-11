b44e59fbd64f871400981e1d9c7bd8a54b2f7050
peterlidee <peter@lidee.be>
2023-05-11 21:08:53 +0200

    Added settingsToggle test

---
+++ components/header/__test__/SettingsToggle.test.js
@@ -0,0 +1,49 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import SettingsToggle from '../SettingsToggle'
+import IconSettings from '../../svgSnippets/IconSettings'
+
+jest.mock('../../svgSnippets/IconSettings')
+const ChildMock = jest.fn()
+
+describe('SettingsToggle', () => {
+  test('It renders', () => {
+    render(
+      <SettingsToggle>
+        <ChildMock />
+      </SettingsToggle>,
+    )
+    expect(
+      screen.getByRole('button', { name: /settings/i }),
+    ).toBeInTheDocument()
+    expect(IconSettings).toHaveBeenCalled()
+    expect(ChildMock).toHaveBeenCalled()
+  })
+
+  test('It toggles on button press', async () => {
+    const { container } = render(
+      <SettingsToggle>
+        <ChildMock />
+      </SettingsToggle>,
+    )
+    const user = userEvent.setup()
+    const button = screen.getByRole('button', { name: /settings/i })
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    const collapse = container.querySelector('.settings__collapse')
+
+    // it is initially closed
+    expect(collapse).toHaveStyle('display:none')
+    expect(button).toHaveClass('settings__button')
+    expect(button).not.toHaveClass('settings__button--active')
+    // it opens on click
+    await user.click(button)
+    expect(collapse).toHaveStyle('display:block')
+    expect(button).toHaveClass('settings__button--active')
+    // test multiple clicks
+    await user.click(button)
+    await user.click(button)
+    await user.click(button)
+    expect(collapse).toHaveStyle('display:none')
+  })
+})

bd4cc50ace0bb6f64b01c7a55571107f7dfc61f3
peterlidee <peter@lidee.be>
2023-05-11 14:38:36 +0200

    Updated test to ts

--- lib/numberFilter/__test__/validateNumbersQuery.test.ts
+++ lib/numberFilter/__test__/validateNumbersQuery.test.ts
@@ -2,33 +2,33 @@ import validateNumbersQuery from '../validateNumbersQuery'
 import validateNumbersAgainstDefaults from '../validateNumbersAgainstDefaults'
 
 jest.mock('../validateNumbersAgainstDefaults', () => {
-  return jest.fn((value1, value2, defaults) => [ value1, value2, defaults ])
+  return jest.fn((value1, value2, defaults) => [value1, value2, defaults])
 })
 
 describe('function validateNumbersQuery', () => {
-  
   test('It returns defaults when !queryValue', () => {
-    expect(validateNumbersQuery(undefined, [1])).toEqual([1])
-    expect(validateNumbersQuery('', [2])).toEqual([2])
-    expect(validateNumbersQuery(0, [3])).toEqual([3])
+    expect(validateNumbersQuery(undefined, [1, 2])).toEqual([1, 2])
+    expect(validateNumbersQuery('', [1, 2])).toEqual([1, 2])
+    expect(validateNumbersQuery(0 as unknown as string, [1, 2])).toEqual([1, 2])
   })
 
   test('It returns defaults when no komma', () => {
-    expect(validateNumbersQuery('123', [4])).toEqual([4])
+    expect(validateNumbersQuery('123', [1, 2])).toEqual([1, 2])
   })
 
   test('It splits queryValue', () => {
-    const result = validateNumbersQuery('10,90', [0]);
+    const result = validateNumbersQuery('10,90', [1, 2])
     expect(result[0]).toBe(10)
     expect(result[1]).toBe(90)
-    expect(result[2]).toEqual([0])
   })
 
   test('It calls validateNumbersAgainstDefaults', () => {
-    validateNumbersQuery('10,90', [50])
+    validateNumbersQuery('10,90', [50, 100])
     expect(validateNumbersAgainstDefaults).toHaveBeenCalledTimes(1)
-    expect(validateNumbersAgainstDefaults).toHaveBeenCalledWith(10,90, [50])
+    expect(validateNumbersAgainstDefaults).toHaveBeenCalledWith(
+      10,
+      90,
+      [50, 100],
+    )
   })
-
 })
-

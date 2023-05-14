41de5e3165701d2466edaa0e2a0c8865a8e24f14
peterlidee <peter@lidee.be>
2023-05-14 17:20:29 +0200

    added SingleCountry type

---
+++ types/singleCountry.d.ts
@@ -0,0 +1,12 @@
+export type SingleCountry = {
+  countryName: string
+  cca3: string
+  capital: string
+  region: string
+  subregion: string
+  borders: string[]
+  area: number
+  population: number
+  flag: string
+  coatOfArms: string
+}

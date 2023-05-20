1a75a9cdd28f773ef9cf5091966c0a077d89f81f
peterlidee <peter@lidee.be>
2023-05-20 17:53:40 +0200

    Updated singleCountryMock

--- __mock__/data/singleCountryMocks.js
+++ __mock__/data/singleCountryMocks.js
@@ -1,103 +1,73 @@
 const singleCountryMocks = [
   {
-    "name": {
-      common: "Algeria",
-    },
-    "cca2": "DZ",
-    "ccn3": "012",
-    "cca3": "DZA",
-    "cioc": "ALG",
-    capital: ["Algiers"],
-    region: "Africa",
-    subregion: "Northern Africa",
-    latlng: [28, 3],
-    borders: ["TUN","LBY","NER","ESH","MRT","MLI","MAR"],
+    countryName: 'Algeria',
+    tld: '.dz',
+    cca2: 'DZ',
+    cca3: 'DZA',
+    capital: 'Algiers',
+    region: 'Africa',
+    subregion: 'Northern Africa',
+    borders: ['TUN', 'LBY', 'NER', 'ESH', 'MRT', 'MLI', 'MAR'],
     area: 2381741,
     population: 44700000,
-    flags: {
-      png: "https://flagcdn.com/w320/dz.png",
-      svg: "https://flagcdn.com/dz.svg"
-    },
-    coatOfArms: {
-      png: "https://mainfacts.com/media/images/coats_of_arms/dz.png",
-      svg: "https://mainfacts.com/media/images/coats_of_arms/dz.svg"
-    },
+    flag: 'https://flagcdn.com/dz.svg',
+    coatOfArms: 'https://mainfacts.com/media/images/coats_of_arms/dz.svg',
   },
   // same country without subregion
   {
-    "name": {
-      common: "Algeria",
-    },
-    "cca2": "DZ",
-    "ccn3": "012",
-    "cca3": "DZA",
-    "cioc": "ALG",
-    capital: ["Algiers"],
-    region: "Africa",
-    // subregion: "Northern Africa",
-    latlng: [28, 3],
-    borders: ["TUN","LBY","NER","ESH","MRT","MLI","MAR"],
+    countryName: 'Algeria',
+    tld: '.dz',
+    cca2: 'DZ',
+    cca3: 'DZA',
+    capital: 'Algiers',
+    region: 'Africa',
+    borders: ['TUN', 'LBY', 'NER', 'ESH', 'MRT', 'MLI', 'MAR'],
     area: 2381741,
     population: 44700000,
-    flags: {
-      png: "https://flagcdn.com/w320/dz.png",
-      svg: "https://flagcdn.com/dz.svg"
-    },
-    coatOfArms: {
-      png: "https://mainfacts.com/media/images/coats_of_arms/dz.png",
-      svg: "https://mainfacts.com/media/images/coats_of_arms/dz.svg"
-    },
+    flag: 'https://flagcdn.com/dz.svg',
+    coatOfArms: 'https://mainfacts.com/media/images/coats_of_arms/dz.svg',
   },
   // same country with no capital
   {
-    "name": {
-      common: "Algeria",
-    },
-    "cca2": "DZ",
-    "ccn3": "012",
-    "cca3": "DZA",
-    "cioc": "ALG",
-    // capital: ["Algiers"],
-    region: "Africa",
-    subregion: "Northern Africa",
-    latlng: [28, 3],
-    borders: ["TUN","LBY","NER","ESH","MRT","MLI","MAR"],
+    countryName: 'Algeria',
+    tld: '.dz',
+    cca2: 'DZ',
+    cca3: 'DZA',
+    region: 'Africa',
+    subregion: 'Northern Africa',
+    borders: ['TUN', 'LBY', 'NER', 'ESH', 'MRT', 'MLI', 'MAR'],
     area: 2381741,
     population: 44700000,
-    flags: {
-      png: "https://flagcdn.com/w320/dz.png",
-      svg: "https://flagcdn.com/dz.svg"
-    },
-    coatOfArms: {
-      png: "https://mainfacts.com/media/images/coats_of_arms/dz.png",
-      svg: "https://mainfacts.com/media/images/coats_of_arms/dz.svg"
-    },
+    flag: 'https://flagcdn.com/dz.svg',
+    coatOfArms: 'https://mainfacts.com/media/images/coats_of_arms/dz.svg',
   },
   // different country
   {
-    "name": {
-      common: "Fantasia",
-    },
-    "cca2": "BE",
-    "ccn3": "056",
-    "cca3": "BEL",
-    "cioc": "BEL",
-    capital: ["Brussels"],
-    region: "Europe",
-    subregion: "Western Europe",
+    countryName: 'Fantasia',
+    cca2: 'BE',
+    cca3: 'BEL',
+    tld: '.be',
+    capital: ['Brussels'],
+    region: 'Europe',
+    subregion: 'Western Europe',
     latlng: [50.83333333, 4],
-    borders: ["FRA","DEU","NLD","LUX"],
+    borders: ['FRA', 'DEU', 'NLD', 'LUX'],
     area: 30528,
     population: 11555997,
-    flags: {
-      png: "https://flagcdn.com/w320/be.png",
-      svg: "https://flagcdn.com/be.svg"
-      
-    },
-    coatOfArms: {
-      png: "https://mainfacts.com/media/images/coats_of_arms/be.png",
-      svg: "https://mainfacts.com/media/images/coats_of_arms/be.svg"
-    },
+    flag: 'https://flagcdn.com/be.svg',
+    coatOfArms: 'https://mainfacts.com/media/images/coats_of_arms/be.svg',
+    countryName: 'Algeria',
+    tld: '.dz',
+    cca2: 'DZ',
+    cca3: 'DZA',
+    capital: 'Algiers',
+    region: 'Africa',
+    subregion: 'Northern Africa',
+    borders: ['TUN', 'LBY', 'NER', 'ESH', 'MRT', 'MLI', 'MAR'],
+    area: 2381741,
+    population: 44700000,
+    flag: 'https://flagcdn.com/dz.svg',
+    coatOfArms: 'https://mainfacts.com/media/images/coats_of_arms/dz.svg',
   },
 ]
 

f2ce6c3e895e1723ea57e2ff2a538da71dda2c22
peterlidee <peter@lidee.be>
2023-05-21 18:11:59 +0200

    Added and converted populationChartWidget + test

--- components/single/chart/PopulationChartWidget.tsx
+++ components/single/chart/PopulationChartWidget.tsx
@@ -1,7 +1,7 @@
 // https://www.chartjs.org/docs/latest/getting-started/integration.html
-import { 
+import {
   Chart as ChartJS,
-  CategoryScale, 
+  CategoryScale,
   LinearScale,
   BarElement,
   BarController,
@@ -10,7 +10,8 @@ import {
   LineController,
   Tooltip,
   Legend,
-} from 'chart.js';
+} from 'chart.js'
+import type { ChartData, ChartOptions } from 'chart.js'
 
 ChartJS.register(
   CategoryScale,
@@ -22,30 +23,43 @@ ChartJS.register(
   LineController,
   Tooltip,
   Legend,
-);
+)
 
-import { Chart } from 'react-chartjs-2';
-import colors from '../../../config/colors';
-import PropTypes from 'prop-types';
-import formatPopulationLabel from '../../../lib/single/formatPopulationLabel';
+import { Chart } from 'react-chartjs-2'
+import { colors } from '../../../config/colors'
+import PropTypes from 'prop-types'
+import formatPopulationLabel from '../../../lib/single/formatPopulationLabel'
 
-function PopulationChartWidget(props){
+type Props = {
+  years: number[]
+  femaleTotal: number[]
+  maleTotal: number[]
+  combinedTotal: number[]
+}
 
-  const chartData = {
-    labels: props.years,
+function PopulationChartWidget({
+  years,
+  femaleTotal,
+  maleTotal,
+  combinedTotal,
+}: Props) {
+  // https://react-chartjs-2.js.org/faq/typescript/
+  // use ChartData generic
+  const chartData: ChartData = {
+    labels: years,
     datasets: [
       {
         type: 'bar',
         label: 'Total females',
-        data: props.femaleTotal,
-        backgroundColor: "rgba(84, 139, 244, 0.5)",
-        hoverBackgroundColor: "rgba(84, 139, 244, 0.5)",
+        data: femaleTotal,
+        backgroundColor: 'rgba(84, 139, 244, 0.5)',
+        hoverBackgroundColor: 'rgba(84, 139, 244, 0.5)',
         barPercentage: 1,
       },
       {
         type: 'bar',
         label: 'Total males',
-        data: props.maleTotal,
+        data: maleTotal,
         backgroundColor: colors.blue,
         hoverBackgroundColor: colors.blue,
         barPercentage: 1,
@@ -53,19 +67,21 @@ function PopulationChartWidget(props){
       {
         type: 'line',
         label: 'Total population',
-        data: props.combinedTotal,
-        backgroundColor: "#52cc7c",
-        hoverBackgroundColor: "#52cc7c",
-        borderColor: "#52cc7c",
-        hoverBorderColor: "#52cc7c",
+        data: combinedTotal,
+        backgroundColor: '#52cc7c',
+        hoverBackgroundColor: '#52cc7c',
+        borderColor: '#52cc7c',
+        hoverBorderColor: '#52cc7c',
         borderWidth: 4,
         pointBorderWidth: 2,
         pointHoverBorderWidth: 4,
       },
-    ]
+    ],
   }
 
-  const chartOptions = {
+  // https://react-chartjs-2.js.org/faq/typescript/
+  // use ChartOptions generic
+  const chartOptions: ChartOptions = {
     responsive: true, // default
     // turn off aspectration, it gets too small on small sizes!!
     maintainAspectRatio: false,
@@ -73,17 +89,17 @@ function PopulationChartWidget(props){
     scales: {
       y: {
         ticks: {
-          callback: function(value, index, values) {
-            return formatPopulationLabel(value);
-          }
-        }
-      }
+          callback: function (value, index, values) {
+            return formatPopulationLabel(value)
+          },
+        },
+      },
     },
     // show the legend on the bottom
     plugins: {
       legend: {
         display: true, // default
-        position: "bottom",
+        position: 'bottom',
         labels: {
           padding: 20,
         },
@@ -96,12 +112,12 @@ function PopulationChartWidget(props){
     },
   }
 
-  const startYear = props.years[0] || "-";
-  const endYear = props.years[props.years.length - 1] || "-";
+  const startYear = years[0] || '-'
+  const endYear = years[years.length - 1] || '-'
 
-  return(
+  return (
     <>
-      <h4 className="chart__title">{`Population evolution ${startYear}-${endYear}`}</h4>
+      <h4 className='chart__title'>{`Population evolution ${startYear}-${endYear}`}</h4>
       <div className='chart-container'>
         <Chart type='bar' data={chartData} options={chartOptions} />
       </div>
@@ -115,4 +131,4 @@ PopulationChartWidget.propTypes = {
   maleTotal: PropTypes.array.isRequired,
   combinedTotal: PropTypes.array.isRequired,
 }
-export default PopulationChartWidget;
+export default PopulationChartWidget

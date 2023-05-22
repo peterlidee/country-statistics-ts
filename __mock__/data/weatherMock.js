19f227acf2478ac672bb0863e4596a67c6d99ee3
peterlidee <peter@lidee.be>
2023-05-22 18:45:35 +0200

    Updated mock + import

--- __mock__/data/weatherMock.js
+++ __mock__/data/weatherMock.js
@@ -1,35 +1,56 @@
-const weatherMocks = [
-  {
-    coord: { lon: 4.3488, lat: 50.8504 },
-    weather: [
-      { id: 803, main: 'Clouds', description: 'broken clouds', icon: '04d' },
-    ],
-    base: 'stations',
-    main: {
-      temp: 18.55,
-      feels_like: 18.47,
-      temp_min: 15.99,
-      temp_max: 21.51,
-      pressure: 1017,
-      humidity: 77,
-    },
-    visibility: 6000,
-    wind: { speed: 3.6, deg: 300 },
-    clouds: { all: 75 },
-    dt: 1684761485,
-    sys: {
-      type: 1,
-      id: 1227,
-      country: 'BE',
-      sunrise: 1684727088,
-      sunset: 1684784038,
-    },
-    timezone: 7200,
-    id: 2800866,
-    name: 'Brussels',
-    cod: 200,
+const validWeatherMock = {
+  coord: { lon: 4.3488, lat: 50.8504 },
+  weather: [
+    { id: 803, main: 'Clouds', description: 'broken clouds', icon: '04d' },
+  ],
+  base: 'stations',
+  main: {
+    temp: 18.55,
+    feels_like: 18.47,
+    temp_min: 15.99,
+    temp_max: 21.51,
+    pressure: 1017,
+    humidity: 77,
   },
-  { cod: '404', message: 'city not found' },
-]
+  visibility: 6000,
+  wind: { speed: 3.6, deg: 300 },
+  clouds: { all: 75 },
+  dt: 1684761485,
+  sys: {
+    type: 1,
+    id: 1227,
+    country: 'BE',
+    sunrise: 1684727088,
+    sunset: 1684784038,
+  },
+  timezone: 7200,
+  id: 2800866,
+  name: 'Brussels',
+  cod: 200,
+}
 
-export { weatherMocks }
+const invalidWeatherMock = { cod: '404', message: 'city not found' }
+
+const invalidForecast = {
+  capitalName: 'abc',
+  description: 'city not found',
+  dayNight: 'day',
+  weather: 'nodata',
+  tempMin: '__',
+  tempMax: '__',
+  windDeg: 90,
+  windSpeed: '__',
+}
+
+const validForecast = {
+  capitalName: 'Brussels',
+  description: 'broken clouds',
+  dayNight: 'day',
+  weather: 'broken',
+  tempMin: 16,
+  tempMax: 22,
+  windDeg: 300,
+  windSpeed: 13,
+}
+
+export { validWeatherMock, invalidWeatherMock, validForecast, invalidForecast }

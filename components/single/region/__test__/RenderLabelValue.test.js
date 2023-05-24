27e229cfb79b3ff828d000b5053502944ea3a3ca
peterlidee <peter@lidee.be>
2023-05-24 16:51:06 +0200

    Removed loading state from renderLabelValue + test

--- components/single/region/__test__/RenderLabelValue.test.js
+++ components/single/region/__test__/RenderLabelValue.test.js
@@ -3,7 +3,7 @@ import RenderLabelValue from '../RenderLabelValue'
 
 describe('components/single/region/RenderLabelValue', () => {
   test('It renders', () => {
-    render(<RenderLabelValue loading={false} label='label' value='value' />)
+    render(<RenderLabelValue label='label' value='value' />)
     expect(screen.getByText(/label/i)).toBeInTheDocument()
     expect(screen.getByText(/value/i)).toBeInTheDocument()
   })
@@ -11,24 +11,16 @@ describe('components/single/region/RenderLabelValue', () => {
   test('It renders with value empty: ""', () => {
     render(<RenderLabelValue loading={false} label='label' value='' />)
     expect(screen.getByText(/label/i)).toBeInTheDocument()
-    expect(screen.getByText(/No Data/i)).toBeInTheDocument()
-  })
-
-  test('It renders loading', () => {
-    render(<RenderLabelValue loading={true} label='label' value='value' />)
-    expect(screen.getByText(/label/i)).toBeInTheDocument()
-    expect(screen.getByText('...')).toBeInTheDocument()
+    expect(screen.getByText(/none/i)).toBeInTheDocument()
   })
 
   test('It rerenders correctly', () => {
     const { rerender } = render(
-      <RenderLabelValue loading={false} label='label' value='first value' />,
+      <RenderLabelValue label='label' value='first value' />,
     )
     expect(screen.getByText(/label/i)).toBeInTheDocument()
     expect(screen.getByText('first value')).toBeInTheDocument()
-    rerender(
-      <RenderLabelValue loading={false} label='label' value='second value' />,
-    )
+    rerender(<RenderLabelValue label='label' value='second value' />)
     expect(screen.getByText('second value')).toBeInTheDocument()
   })
 })

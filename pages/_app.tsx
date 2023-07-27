089cc96a94f2c53e4d30fbcdafbbd2acb7201518
peterlidee <peter@lidee.be>
2023-07-27 15:34:50 +0200

    Added RQ provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,16 @@ import 'normalize.css'
 import '../styles/index.scss'
 import Page from '@/components/Page'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <QueryClientProvider client={queryClient}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </QueryClientProvider>
   )
 }

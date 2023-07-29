8373dffe4c782c41dfbbe9aa3eb29087e70b6cd7
peterlidee <peter@lidee.be>
2023-07-29 12:16:26 +0200

    Added test for WeatherComponent

---
+++ components/single/weather/__test__/WeatherComponent.test.js
@@ -0,0 +1,67 @@
+import { render } from '@testing-library/react'
+
+import WeatherComponent from '../WeatherComponent'
+import SingleCountryComponent from '../../SingleCountryComponent'
+import { useWeatherData } from '../useWeatherData'
+import WeatherWidget from '../WeatherWidget'
+
+jest.mock('../useWeatherData')
+jest.mock('../../SingleCountryComponent')
+SingleCountryComponent.mockImplementation((props) => props.children)
+jest.mock('../WeatherWidget')
+
+function setup(
+  cca2 = 'aaa',
+  capitalName = 'BBBbbb',
+  data = { data: 'ccc' },
+  error = undefined,
+  isLoading = false,
+) {
+  useWeatherData.mockReturnValue({
+    data,
+    error,
+    isLoading,
+  })
+  render(<WeatherComponent cca2={cca2} capitalName={capitalName} />)
+}
+
+describe('components/single/weather/WeatherComponent', () => {
+  test('It renders', () => {
+    setup()
+    expect(useWeatherData).toHaveBeenCalled()
+    expect(SingleCountryComponent).toHaveBeenCalled()
+    expect(WeatherWidget).toHaveBeenCalled()
+  })
+
+  test('useWeatherData mock gets called with the correct props', () => {
+    setup()
+    expect(useWeatherData).toHaveBeenCalledWith(
+      'aaa',
+      'https://api.openweathermap.org/data/2.5/weather?q=BBBbbb,aaa&APPID=undefined&units=metric',
+    )
+  })
+
+  test('SingleCountryComponent mock gets called with the correct props', () => {
+    setup()
+    expect(SingleCountryComponent).toHaveBeenCalledWith(
+      expect.objectContaining({
+        error: undefined,
+        loading: false,
+        endpoint:
+          'https://api.openweathermap.org/data/2.5/weather?q=BBBbbb,aaa&APPID=undefined&units=metric',
+      }),
+      expect.anything(),
+    )
+  })
+
+  test('WeatherWidget get called with the correct props', () => {
+    setup()
+    expect(WeatherWidget).toHaveBeenCalledWith(
+      expect.objectContaining({
+        data: 'ccc',
+        code: 'aaa',
+      }),
+      expect.anything(),
+    )
+  })
+})

eb99f89c7ab2566144fa114d8be54a495be83de1
peterlidee <peter@lidee.be>
2023-07-31 21:15:56 +0200

    Added ChartComponent + test

---
+++ components/single/chart/ChartComponent.tsx
@@ -0,0 +1,49 @@
+import { useChartData } from './useChartData'
+import compilePopulationData from '@/lib/single/compilePopulationData'
+import Source from '@/components/sources/Source'
+import SingleCountryComponent from '../SingleCountryComponent'
+import PopulationChartWidget from './PopulationChartWidget'
+
+type Props = {
+  countryCode: string
+}
+
+export default function ChartComponent({ countryCode }: Props) {
+  const endpoint = `https://api.worldbank.org/v2/country/${countryCode.toLowerCase()}/indicator/SP.POP.TOTL.FE.IN;SP.POP.TOTL.MA.IN?format=json&source=2&date=2002:2021&per_page=100`
+  const label =
+    'api.worldbank.org/{country} Health Nutrition and Population Statistics'
+
+  // make fetch
+  const { data, isLoading, error, isError } = useChartData(
+    countryCode,
+    endpoint,
+  )
+
+  // data -> calculate labels and values
+  const {
+    populationData: { years, femaleTotal, maleTotal, combinedTotal },
+    extraError,
+  } = compilePopulationData(data?.data, isError)
+
+  // construct sources
+  const sources = [
+    <Source
+      key='chartSource1'
+      error={(error as Error | undefined) || extraError}
+      loading={isLoading}
+      endpoint={endpoint}
+      label={label}
+    />,
+  ]
+
+  return (
+    <SingleCountryComponent extraClass='population-chart' sources={sources}>
+      <PopulationChartWidget
+        years={years}
+        femaleTotal={femaleTotal}
+        maleTotal={maleTotal}
+        combinedTotal={combinedTotal}
+      />
+    </SingleCountryComponent>
+  )
+}
---
+++ components/single/chart/__test__/ChartComponent.test.js
@@ -0,0 +1,93 @@
+import { render } from '@testing-library/react'
+
+import ChartComponent from '../ChartComponent'
+import { useChartData } from '../useChartData'
+import compilePopulationData from '../../../../lib/single/compilePopulationData'
+import Source from '../../../sources/Source'
+import SingleCountryComponent from '../../SingleCountryComponent'
+import PopulationChartWidget from '../PopulationChartWidget'
+
+jest.mock('../useChartData')
+jest.mock('../../../../lib/single/compilePopulationData')
+jest.mock('../../../sources/Source')
+jest.mock('../../SingleCountryComponent')
+jest.mock('../PopulationChartWidget')
+
+useChartData.mockReturnValue({
+  data: { data: 'foobar' },
+  isLoading: false,
+  error: undefined,
+  isError: false,
+})
+compilePopulationData.mockReturnValue({
+  populationData: {
+    years: [2000, 2001, 2002],
+    femaleTotal: [10, 20, 30],
+    maleTotal: [11, 22, 33],
+    combinedTotal: [55, 66, 77],
+  },
+  extraError: undefined,
+})
+SingleCountryComponent.mockImplementation((props) => (
+  <>
+    {props.children}
+    {props.sources.map((source) => source)}
+  </>
+))
+
+describe('components/single/chart/ChartComponent', () => {
+  test('It renders', () => {
+    render(<ChartComponent countryCode='AAA' />)
+    expect(useChartData).toHaveBeenCalled()
+    expect(compilePopulationData).toHaveBeenCalled()
+    expect(Source).toHaveBeenCalled()
+    expect(SingleCountryComponent).toHaveBeenCalled()
+    expect(PopulationChartWidget).toHaveBeenCalled()
+  })
+  test('It calls useChartData mock with the correct props', () => {
+    render(<ChartComponent countryCode='AAA' />)
+    expect(useChartData).toHaveBeenCalledWith(
+      'AAA',
+      'https://api.worldbank.org/v2/country/aaa/indicator/SP.POP.TOTL.FE.IN;SP.POP.TOTL.MA.IN?format=json&source=2&date=2002:2021&per_page=100',
+    )
+  })
+  test('It calls compilePopulationData mock with the correct props', () => {
+    render(<ChartComponent countryCode='AAA' />)
+    expect(compilePopulationData).toHaveBeenCalledWith('foobar', false)
+  })
+  test('It calls Sources mock with the correct props', () => {
+    render(<ChartComponent countryCode='AAA' />)
+    expect(Source).toHaveBeenCalledWith(
+      expect.objectContaining({
+        error: undefined,
+        loading: false,
+        endpoint:
+          'https://api.worldbank.org/v2/country/aaa/indicator/SP.POP.TOTL.FE.IN;SP.POP.TOTL.MA.IN?format=json&source=2&date=2002:2021&per_page=100',
+        label:
+          'api.worldbank.org/{country} Health Nutrition and Population Statistics',
+      }),
+      expect.anything(),
+    )
+  })
+  test('It calls SingleCountryComponent mock with the correct props', () => {
+    render(<ChartComponent countryCode='AAA' />)
+    expect(SingleCountryComponent).toHaveBeenCalledWith(
+      expect.objectContaining({
+        extraClass: 'population-chart',
+      }),
+      expect.anything(),
+    )
+  })
+  test('It calls PopulationChartWidget mock with the correct props', () => {
+    render(<ChartComponent countryCode='AAA' />)
+    expect(PopulationChartWidget).toHaveBeenCalledWith(
+      expect.objectContaining({
+        years: [2000, 2001, 2002],
+        femaleTotal: [10, 20, 30],
+        maleTotal: [11, 22, 33],
+        combinedTotal: [55, 66, 77],
+      }),
+      expect.anything(),
+    )
+  })
+})

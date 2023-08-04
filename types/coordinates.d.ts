9cf4686d99e9e4a2f4e296e709ef924aef1a2f37
peterlidee <peter@lidee.be>
2023-08-04 17:35:38 +0200

    Added Coordinates types

--- types/coordinates.d.ts
+++ types/coordinates.d.ts
@@ -1,1 +1,9 @@
 export type Coordinates = [number, number]
+export type RegionCoordinates = {
+  coordinates: Coordinates[]
+  endpoint: string
+}
+export type CoordinatesData = {
+  region: RegionCoordinates
+  subregion: RegionCoordinates
+}

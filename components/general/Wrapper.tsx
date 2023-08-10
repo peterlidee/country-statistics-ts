98d7bd484857105e5997aad7ca754dd73f9adc4e
peterlidee <peter@lidee.be>
2023-08-10 15:05:23 +0200

    Updated Wrapper with children optional + updated test to test children as nodes, not elements

--- components/general/Wrapper.tsx
+++ components/general/Wrapper.tsx
@@ -1,19 +1,20 @@
-/**
- * Wraps {children} in div.base.base--modifier
- * @param props.base - the base class
- * @param props.modifier - modifier of base class: base--modifier
- * @param props.children
- * @returns JSX.Element <div class="{base} {base}--{modifier}">{props.children}</div>
- */
+import PropTypes from 'prop-types'
 
 type WrapperProps = {
   base: string
   modifier: string
-  children: React.ReactNode
+  children?: React.ReactNode
 }
 
+// Wraps {children} in div.base.base--modifier
 function Wrapper({ base, modifier, children }: WrapperProps) {
   return <div className={`${base} ${base}--${modifier}`}>{children}</div>
+}
+
+Wrapper.propTypes = {
+  base: PropTypes.string.isRequired,
+  modifier: PropTypes.string.isRequired,
+  children: PropTypes.node,
 }
 
 export default Wrapper
--- components/general/__test__/Wrapper.test.js
+++ components/general/__test__/Wrapper.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable testing-library/no-node-access */
 /* eslint-disable testing-library/no-container */
 
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import Wrapper from '../Wrapper'
 
@@ -10,7 +10,7 @@ const ChildMock = jest.fn()
 describe('components/general/Wrapper', () => {
   test('It renders', () => {
     const { container } = render(<Wrapper base='base' modifier='modifier' />)
-    expect(container.querySelector('div')).toBeInTheDocument()
+    expect(container.querySelector('.base--modifier')).toBeInTheDocument()
   })
 
   test('It prints the correct classes', () => {
@@ -18,12 +18,53 @@ describe('components/general/Wrapper', () => {
     expect(container.querySelector('div')).toHaveClass('base base--modifier')
   })
 
-  test('It passes children correctly', () => {
-    render(
-      <Wrapper base='base' modifier='modifier'>
-        <ChildMock />
-      </Wrapper>,
-    )
-    expect(ChildMock).toHaveBeenCalled()
+  test('The children are optional', () => {
+    const { container } = render(<Wrapper base='base' modifier='modifier' />)
+    const wrapperEl = container.querySelector('.base--modifier')
+    expect(wrapperEl).toBeInTheDocument()
+    expect(wrapperEl).toBeEmptyDOMElement()
+  })
+
+  describe('Children are nodes or elements, does not matter', () => {
+    test('It passes element correctly', () => {
+      render(
+        <Wrapper base='base' modifier='modifier'>
+          <ChildMock />
+        </Wrapper>,
+      )
+      expect(ChildMock).toHaveBeenCalled()
+    })
+    test('It passes string correctly', () => {
+      render(
+        <Wrapper base='base' modifier='modifier'>
+          foobar
+        </Wrapper>,
+      )
+      expect(screen.getByText(/foobar/)).toBeInTheDocument()
+    })
+    test('It passes number correctly', () => {
+      render(
+        <Wrapper base='base' modifier='modifier'>
+          123456
+        </Wrapper>,
+      )
+      expect(screen.getByText('123456')).toBeInTheDocument()
+    })
+    test('It passes undefined correctly', () => {
+      const { container } = render(<Wrapper base='base' modifier='modifier' />)
+      const wrapperEl = container.querySelector('.base--modifier')
+      expect(wrapperEl).toBeInTheDocument()
+      expect(wrapperEl).toBeEmptyDOMElement()
+    })
+    test('It passes null correctly', () => {
+      const { container } = render(
+        <Wrapper base='base' modifier='modifier'>
+          {null}
+        </Wrapper>,
+      )
+      const wrapperEl = container.querySelector('.base--modifier')
+      expect(wrapperEl).toBeInTheDocument()
+      expect(wrapperEl).toBeEmptyDOMElement()
+    })
   })
 })

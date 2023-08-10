d6a1d8a48663368a08880f6ca4c357d4d3fa5105
peterlidee <peter@lidee.be>
2023-08-10 14:34:59 +0200

    Updated test with required propType

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,22 +1,24 @@
 import { render } from '@testing-library/react'
 
-import HomePage from "../pages"
+import HomePage from '../pages'
 import Home from '../components/Home'
 
 jest.mock('../components/Home')
 
 describe('page/index.js', () => {
   test('It renders because Home was called', () => {
-    render(<HomePage />)
+    render(<HomePage endpoint='foobar' />)
     expect(Home).toHaveBeenCalled()
   })
   test('Home mock is called with the correct props', () => {
-    render(<HomePage countries={[1,2,3]} endpoint="url" filterData={{}} />)
+    render(<HomePage countries={[1, 2, 3]} endpoint='url' filterData={{}} />)
     expect(Home).toHaveBeenCalledWith(
       expect.objectContaining({
-      endpoint: 'url',
-      countries: [1,2,3],
-      filterData: expect.anything(),
-    }), expect.anything())
+        endpoint: 'url',
+        countries: [1, 2, 3],
+        filterData: expect.anything(),
+      }),
+      expect.anything(),
+    )
   })
 })

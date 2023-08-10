704f2f90cd6e51bd748934b0969176bfb8aab154
peterlidee <peter@lidee.be>
2023-08-10 14:31:05 +0200

    Added filterData PropTypes

---
+++ propTypes/filterDataPropTypes.ts
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+
+const numberFilterDataPropTypes = PropTypes.exact({
+  sliderStart: PropTypes.number.isRequired,
+  sliderEnd: PropTypes.number.isRequired,
+  sliderStep: PropTypes.number.isRequired,
+  countryMin: PropTypes.number.isRequired,
+  countryMax: PropTypes.number.isRequired,
+})
+
+const defaultRegionStatePropTypes = PropTypes.objectOf(
+  PropTypes.objectOf(PropTypes.arrayOf(PropTypes.string)),
+)
+
+const regionIndexesPropTypes = PropTypes.objectOf(
+  PropTypes.arrayOf(PropTypes.number),
+)
+
+const filterDataPropTypes = PropTypes.exact({
+  defaultRegionState: defaultRegionStatePropTypes.isRequired,
+  regionIndexes: regionIndexesPropTypes.isRequired,
+  area: numberFilterDataPropTypes.isRequired,
+  density: numberFilterDataPropTypes.isRequired,
+  population: numberFilterDataPropTypes.isRequired,
+})
+
+export { filterDataPropTypes }

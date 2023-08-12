f85c3bcb7403ee35cfec86f9fc258940c29db8dc
peterlidee <peter@lidee.be>
2023-08-12 15:23:33 +0200

    Added propTypes

--- components/single/region/RenderLabelValue.tsx
+++ components/single/region/RenderLabelValue.tsx
@@ -1,20 +1,21 @@
-/**
- * renders label and value, with empty value option
- * @param props.value
- * @param props.label
- * @returns React.ReactNode
- */
+import PropTypes from 'prop-types'
 
 type Props = {
   value: string
   label: string
 }
 
+// renders label and value, with empty value option
 const RenderLabelValue = ({ value, label }: Props) => (
   <>
     <div className='single-country__label'>{label}</div>
     <div className='single-country__value'>{value === '' ? 'None' : value}</div>
   </>
 )
+
+RenderLabelValue.propTypes = {
+  value: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+}
 
 export default RenderLabelValue

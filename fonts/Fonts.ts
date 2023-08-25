0abb170b24b78f64790632526fa5e626fd47852f
peterlidee <peter@lidee.be>
2023-08-25 14:43:23 +0200

    Added Fonts with Inconsolata and Quicksand

---
+++ fonts/Fonts.ts
@@ -0,0 +1,4 @@
+import { Inconsolata, Quicksand } from 'next/font/google'
+
+export const inconsolata = Inconsolata({ subsets: ['latin'] })
+export const quicksand = Quicksand({ subsets: ['latin'] })

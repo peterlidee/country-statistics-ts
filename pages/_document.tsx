743cfe38643ec76881bb0939b8161a06349d88c9
peterlidee <peter@lidee.be>
2023-08-25 16:41:33 +0200

    Removed old manuel import

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,8 @@
-// pages/_document.js
-// https://nextjs.org/docs/messages/no-page-custom-font
-// TODO: remove old import
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head>
-        {/* TODO
-          todso
-         <link
-          href='https://fonts.googleapis.com/css2?family=Inconsolata&family=Quicksand:wght@400;500;700&display=swap'
-          rel='stylesheet'
-        /> */}
-      </Head>
       <body>
         <Main />
         <NextScript />
